7b5425d00f27d77ff813a5d54982d1c991541254
Maja Jelenik <maja.jelenik@vp.pl>
2018-08-08 01:25:55 +0200

    Add files

---
+++ calculateStylePoints.js
@@ -0,0 +1,5 @@
+const calculateStylePoints = (styleNotes) => {
+  const max = Math.max(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+  const min = Math.min(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+    return styleNotes[0] + styleNotes[1] + styleNotes[2] + styleNotes[3] + styleNotes[4] - max - min;
+};
